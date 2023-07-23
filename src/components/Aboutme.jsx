ad416efb4ef396eaff78362b23738fbebd7fe956
Will Certeza <wcerteza@gmail.com>
2023-07-23 12:38:53 -0500

    added changes to portfolio

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -7,17 +7,28 @@ const Aboutme = () => {
         <SectionIntro>ABOUT ME</SectionIntro>
         <div className="section-content">
           <p className="about-p">
-            I am a solutions driven <strong>Full Stack</strong> developer
+            With a background in the rehabilitation field, I developed a passion
+            for helping others, displaying strong leadership qualities, and
+            fostering effective communication with individuals from diverse
+            backgrounds and ethnicities. Transitioning into software engineering
+            through a bootcamp, I now apply my solutions-driven mindset to
+            create innovative <strong>full-stack</strong> solutions that align
+            with my newfound passion.
           </p>
           <p className="about-p">
-            I originally worked in the rehabilitation industry helping others
-            recover but wanted to pursue my passion software development
+            Besides coding, you can often find me spending quality time with my
+            furry companion, my adorable dog! Training my dog has taught me so
+            much about <strong>patience</strong> and{' '}
+            <strong>consistency</strong>, skills that I bring to my work as
+            well.
           </p>
           <p className="about-p">
-            I love to create, grow, and continue working on myself
-          </p>
-          <p className="about-p">
-            Check out my projects below to see what I can do!
+            The journey from rehabilitation to software engineering might seem
+            like a big leap, but it was fueled by my{' '}
+            <strong>unwavering passion</strong> for coding. I attended a
+            bootcamp, and it was the best decision I ever made! I enjoy bringing
+            my diverse skill set and excellent communication skills to every
+            project, making sure we achieve our goals together.
           </p>
         </div>
         <div className="about-icons">
